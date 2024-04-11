5d37123387d92819cedaa92bbfd7bfd407704062
Rellganss <11211029@student.itk.ac.id>
2024-04-11 23:15:31 +0800

    new

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,16 @@
 // Card.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Card = ({ tree }) => {
-  const handleCardClick = () => {
-    const treePageUrl = `/pohon-deploy/tree/${encodeURIComponent(tree.id)}`;
-    window.open(treePageUrl, '_blank');
-  };
-
   return (
-    <div className="bg-white shadow-md rounded-md p-4 cursor-pointer" onClick={handleCardClick}>
-      <img src={"/pohon-deploy/" + tree.image} alt={tree.name} className="w-full h-40 object-cover rounded-md mb-4" />
-      <h2 className="text-xl font-bold mb-2">{tree.name}</h2>
-      <p className="text-gray-600 mb-2">{tree.latinName}</p>
-      <p className="text-gray-700">{tree.description}</p>
+    <div className="bg-white shadow-md rounded-md p-4">
+      <Link to={`/pohon-deploy/tree/${tree.id}`} className="cursor-pointer">
+        <img src={`/pohon-deploy/${tree.image}`} alt={tree.name} className="w-full h-40 object-cover rounded-md mb-4" />
+        <h2 className="text-xl font-bold mb-2">{tree.name}</h2>
+        <p className="text-gray-600 mb-2">{tree.latinName}</p>
+        <p className="text-gray-700">{tree.description}</p>
+      </Link>
     </div>
   );
 };
