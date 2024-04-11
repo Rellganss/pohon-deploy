16540d7cb5dc32001d2c460f809550fa9e00286e
Arwin Marinta <arwinmarinta3@gmail.com>
2024-04-12 01:01:58 +0800

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,15 @@
 // Card.jsx
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Card = ({ tree }) => {
   return (
     <div className="bg-white shadow-md rounded-md p-4">
-      <Link to={`/pohon-deploy/tree/${tree.id}`} className="cursor-pointer">
-        <img src={`/pohon-deploy/${tree.image}`} alt={tree.name} className="w-full h-40 object-cover rounded-md mb-4" />
+      <Link to={`/tree/${tree.id}`} className="cursor-pointer">
+        <img
+          src={`/${tree.image}`}
+          alt={tree.name}
+          className="w-full h-40 object-cover rounded-md mb-4"
+        />
         <h2 className="text-xl font-bold mb-2">{tree.name}</h2>
         <p className="text-gray-600 mb-2">{tree.latinName}</p>
         <p className="text-gray-700">{tree.description}</p>
